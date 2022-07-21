95ca6e012280326bd0b528237fc6aa8277a9d03d
Sam Moore <sammooresemail23@gmail.com>
2022-07-21 16:25:18 -0700

    Questions array made

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// Questions Array
+let questions = [
+    {
+        question: "The div element is what exactly?",
+        choices: ["A a container", "B a divider", "C a function", "D a page"],
+        answer: "A a container"
+    },
+    {
+        question: "What symbol represents a terminator?",
+        choices: ["A a ';'", "B a '%'", "C a '&'", "D a '*'"],
+        answer: "A a ';'"
+    },
+    {
+        question: "I'm glad I have _______ to apply the functionality.",
+        choices: ["A HTML", "B CSS", "C  Javascript", "D Java"],
+        answer: "C Javascript"
+    },
+    {
+        question: "What does the var command do?",
+        choices: ["A  creates a document", "B creates a variable", "C creates a function", "creates parameters"],
+        answer: "B creates a variable"
+    }]
